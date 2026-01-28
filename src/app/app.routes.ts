720bc972fe5c477742facd341cd3bbe5319a337f
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-01-28 13:32:22 +0100

    Feat: Now Gallery is beeing served on route /gallery.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,4 +2,12 @@ import { Routes } from '@angular/router';
 import {Gallery} from './pages/gallery/gallery';
 
 export const routes: Routes = [
+  {
+    path: 'gallery',
+    component: Gallery
+  },
+  {
+    path: "**",
+    redirectTo: "gallery"
+  }
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import {Gallery} from './pages/gallery/gallery';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Gallery],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
