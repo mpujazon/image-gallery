2f262d9deb8b68a3d1aaed485572a694d0d7920b
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-02-03 12:24:43 +0100

    Feature: Delete icon appearing on hover in desktop devices, and appearing on click in touch devices

--- src/app/components/image-item/image-item.ts
+++ src/app/components/image-item/image-item.ts
@@ -19,10 +19,20 @@ export class ImageItem {
   image = input.required<Image>();
   priority = input.required<boolean>();
   isFeatured = input.required<boolean>();
+
   deleteImageId = output<string>();
+
+  showDeleteButton = signal(false);
+  isTouchDevice = !window.matchMedia('(hover: hover)').matches;
 
   onDeleteClick(event: Event) {
     event.stopPropagation();
     this.deleteImageId.emit(this.image().id);
+  }
+
+  onImageClick(){
+    if (this.isTouchDevice){
+      this.showDeleteButton.update(value => !value);
+    }
   }
 }
