0b984e0a595569e6da005c36de75cf4e75e6e591
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-02-03 11:55:20 +0100

    Refactor: Styles on host component image-item are being applied from this component instead from his father component

--- src/app/components/image-item/image-item.ts
+++ src/app/components/image-item/image-item.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, input, output} from '@angular/core';
+import {ChangeDetectionStrategy, Component, computed, input, output, signal} from '@angular/core';
 import {Image} from '../../interfaces/image';
 import {NgOptimizedImage} from '@angular/common';
 
@@ -7,12 +7,18 @@ import {NgOptimizedImage} from '@angular/common';
   imports: [
     NgOptimizedImage,
   ],
+  host:{
+    '[class.col-span-2]': 'isFeatured()',
+    '[class.row-span-2]': 'isFeatured()'
+
+  },
   templateUrl: './image-item.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ImageItem {
   image = input.required<Image>();
   priority = input.required<boolean>();
+  isFeatured = input.required<boolean>();
   deleteImageId = output<string>();
 
   onDeleteClick(event: Event) {
--- src/app/pages/gallery/gallery.ts
+++ src/app/pages/gallery/gallery.ts
@@ -1,4 +1,4 @@
-import {Component, signal} from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
 import {Image} from '../../interfaces/image';
 import {ImageItem} from '../../components/image-item/image-item';
 
@@ -162,6 +162,7 @@ export class Gallery {
       alt: "Random image 30"
     }
   ]);
+  featuredImageId = signal<string>(this.images()[0].id || '');
 
   removeImage(id: string){
     window.confirm(`Are you sure you want to delete the image with id ${id}?`)?
